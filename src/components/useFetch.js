fcd903f52083df306b9d001a81d165953a44c16b
Alvin Bjelkendal <ab225ki@student.lnu.se>
2021-11-25 11:15:25 +0100

    Add useFetch component

---
+++ src/components/useFetch.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const useFetch = (url) => {
+  const [responseData, setResponseData] = useState([])
+	const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+			async function getData() {
+				try {
+					const response = await axios.get(url)
+          if (response.status !== 200) {
+            throw Error('Could not fetch the data from that resource')
+          } 
+					setResponseData(response.data)
+          setError(null)
+          setIsLoading(false)
+				} catch (error) {
+					setError(error)
+          setIsLoading(false)
+				}
+			}
+			getData()
+		}, 500)
+  }, [url])
+
+  return { responseData, error, isLoading, setResponseData }
+}
+
+export default useFetch
