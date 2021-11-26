864fb30af6e0a81f5d629032c491be3177be9906
Alvin Bjelkendal <ab225ki@student.lnu.se>
2021-11-26 11:23:42 +0100

    Add: Cast slider on movie page

--- src/components/MoviePageDetails.js
+++ src/components/MoviePageDetails.js
@@ -1,20 +1,52 @@
 import { img_300 } from "../config/config";
-import { IonImg } from "@ionic/react";
+import { IonImg, IonText, IonChip } from "@ionic/react";
+import useFetch from "./useFetch";
+import { useParams } from "react-router";
+import { specificMovie } from "../config/config";
+import LoadingSpinner from "./LoadingSpinner";
+import { Swiper, SwiperSlide } from 'swiper/react/swiper-react.js';
 
 const MoviePageDetails = (props) => {
+  const { id } = useParams();
+  const { responseData: credits, error, isLoading } = useFetch(`${specificMovie}${id}/credits`);
+  console.log(credits);
 
   const displayRuntimeInHoursAndMin = () => {
     return (Math.floor(props.runtime / 60) + 'h ' + props.runtime % 60 + 'min')
   }
 
-  return (
-    <div className="movie-page-details">
-      <h3>{props.title}</h3>
-      <p>{props.release_date + " " + displayRuntimeInHoursAndMin()}</p>
-      <div className="movie-page-overview">
-      <img className="movie-page-poster" src={`${img_300}/${props.poster_path}`} alt="poster"></img>
-      <p>{props.overview}</p>
+  const displayTopActors = () => {
+    return (
+      <div className="movie-page-actors">
+        <IonText>Cast</IonText> 
+        <Swiper slidesPerView={3}>
+          {
+            credits && credits.cast.map((actor) => (
+              <SwiperSlide>
+                <IonChip outline={true} color="white" className="actor-chip">{actor.name}</IonChip>
+              </SwiperSlide>
+            ))
+          }
+        </Swiper>
       </div>
+    )
+  }
+
+  return (
+    <div>
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <div className="movie-page-details">
+          <h3>{props.title}</h3>
+          <IonText>{props.release_date + " " + displayRuntimeInHoursAndMin()}</IonText>
+          <div className="movie-page-overview">
+            <img className="movie-page-poster" src={`${img_300}/${props.poster_path}`} alt="poster"></img>
+            <IonText>{props.overview}</IonText>
+          </div>
+          {displayTopActors()}
+        </div>
+      )}
     </div>
   )
 };
