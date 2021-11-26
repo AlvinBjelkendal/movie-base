45ae8fb334bf36b9f63e42736568aaea232c3380
Alvin Bjelkendal <ab225ki@student.lnu.se>
2021-11-26 15:24:14 +0100

    Addd: Popover to show full movie text

--- src/components/MoviePageDetails.js
+++ src/components/MoviePageDetails.js
@@ -1,13 +1,15 @@
 import { avatarPlaceholder, img_300 } from "../config/config";
-import { IonImg, IonText, IonChip, IonLabel, IonIcon, IonBadge } from "@ionic/react";
+import { IonImg, IonText, IonChip, IonLabel, IonIcon, IonBadge, IonButton, IonModal, IonPopover } from "@ionic/react";
 import useFetch from "./useFetch";
 import { useParams } from "react-router";
 import { specificMovie } from "../config/config";
 import LoadingSpinner from "./LoadingSpinner";
 import MoviePageActors from "./MoviePageActors";
+import { useState } from "react";
 
 const MoviePageDetails = (props) => {
   const { id } = useParams();
+  const [showModal, setShowModal] = useState(false);
   const { responseData: credits, error, isLoading } = useFetch(`${specificMovie}${id}/credits`);
 
   const displayRuntimeInHoursAndMin = () => {
@@ -31,6 +33,20 @@ const MoviePageDetails = (props) => {
     )
   }
 
+  const displayFullText = () => {
+    return (
+      <IonPopover cssClass="movie-popover"
+        isOpen={showModal}
+        swipeToClose={true}
+        backdropDismiss={true}
+      >
+        <div className="movie-popover-text">
+          <IonText>{props.overview}</IonText>
+        </div>
+      </IonPopover>
+    )
+  }
+
   return (
     <div>
       {error && <div>{error}</div>}
@@ -38,6 +54,7 @@ const MoviePageDetails = (props) => {
         <LoadingSpinner />
       ) : (
         <div className="movie-page-details">
+          {displayFullText()}
           <h3>{props.title}</h3>
           <div className="movie-page-date-runtime-genres">
             <IonText className="date-runtime">{props.release_date + " " + displayRuntimeInHoursAndMin()}</IonText>
@@ -49,6 +66,7 @@ const MoviePageDetails = (props) => {
             <img className="movie-page-poster" src={`${img_300}/${props.poster_path}`} alt="poster"></img>
             <IonText className="movie-page-text">{props.overview}</IonText>
           </div>
+          <IonButton onClick={() => setShowModal(true)} fill="clear" size="small" className="show-full-text">Show more..</IonButton>
           <MoviePageActors {...credits} />
         </div>
       )}
