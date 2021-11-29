0d95d20474e0d492049fb85606c0841c40346e43
Alvin Bjelkendal <ab225ki@student.lnu.se>
2021-11-29 17:29:18 +0100

    Begin on search functionality

--- src/components/MainPages/Search.js
+++ src/components/MainPages/Search.js
@@ -1,15 +1,24 @@
 import { IonHeader, IonToolbar, IonSearchbar, IonContent } from "@ionic/react";
 import { React, useState } from "react";
 import GenresList from "../Genres/GenresList";
+import useFetch from "../useFetch";
 
 const Search = () => {
   const [searchText, setSearchText] = useState('')
+  const { responseData: movies, error, isLoading } = useFetch(`https://api.themoviedb.org/3/search/movie?&query=${searchText}`)
+  const { responseData: shows } = useFetch(`https://api.themoviedb.org/3/search/tv?&query=${searchText}`)
+
 
   return (
     <IonContent>
       <IonHeader translucent={true}>
         <IonToolbar>
-          <IonSearchbar className="searchbar" value={searchText} placeholder={"Search Moviebase"} animated ></IonSearchbar>
+          <IonSearchbar
+            className="searchbar"
+            value={searchText}
+            placeholder={"Search Titles"}
+            onIonChange={((e) => (setSearchText(e.detail.value)))}
+            animated ></IonSearchbar>
         </IonToolbar>
       </IonHeader>
       <GenresList type="movie" />
